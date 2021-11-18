c0c71776a8a0da09e89a06480edf86c57c2e9df1
antic91 <zeljkoantic91@icloud.com>
2021-11-19 00:32:02 +0100

    Chat***

---
+++ scripts/chat.js
@@ -0,0 +1,292 @@
+/******************************Code for chat********************************************* */
+
+
+
+var messagesArray = [];
+firstAppear = true;
+
+function createChat() {
+    /*Creating wrapper adding id, class and animation*/
+    const chatWrapper = document.createElement("div");
+    chatWrapper.id = "chatWrapper";
+    chatWrapper.classList.add("flex");
+    chatWrapper.classList.add("wrapperAnim");
+
+    /*creating wrapper for messages*/
+    var messagesWrap = document.createElement("div");
+    messagesWrap.id = "mesagesWrapper";
+
+    /*creating wrapper for button and input and creating also button and input*/
+    var inpButtWrapper = document.createElement("div");
+
+    var input1 = document.createElement("input");
+    input1.id = "inputMessage";
+    input1.className = "inputWrite";
+    input1.placeholder = "Write your message here";
+
+    var button = document.createElement("button");
+    button.id = "sendBtn";
+    button.className = "buttonSend";
+    button.textContent = "Send message";
+
+    /*Adding click event on button and calling server*/
+    button.onclick = () => {
+        sendMessage();
+    }
+
+    chatWrapper.appendChild(messagesWrap);
+    chatWrapper.appendChild(inpButtWrapper);
+
+
+    /*Adding animation with delay... after wrapper has been shown*/
+    setTimeout(() => {
+        messagesWrap.className = "messagesWrap flex opacityChat1";
+    
+        inpButtWrapper.className = "InpButt flex opacityChatInputButt";
+
+        inpButtWrapper.appendChild(input1);
+        inpButtWrapper.appendChild(button);
+
+    }, 1800);
+
+    getAllMessages()
+    setInt()
+    return chatWrapper
+}
+
+
+/*OBJECT to create messages*/
+function UserMessage(name, date, message) {
+    this.name = name;
+    this.date = date;
+    this.message = message;
+        
+    this.oneMessage = () => {
+        /*Message wrapper*/
+        const spanWrapper = document.createElement("span");
+        spanWrapper.className = "spanWrapper flex";
+
+        /*Left side with username and time*/
+        const leftSide = document.createElement("span");
+        leftSide.className = "leftSide flex";
+
+        const h3 = document.createElement("h3");
+        h3.textContent = this.name;
+
+        const h6 = document.createElement("h6");
+        h6.textContent = this.date;
+
+        /*Right side with username and time*/
+        const rightSide = document.createElement("span");
+
+        /*if user == antic set button to delete message*/
+        if (user == "antic") {
+            var deleteButton = document.createElement("button");
+        }
+        
+        spanWrapper.appendChild(leftSide);
+        spanWrapper.appendChild(rightSide);
+
+        if (firstAppear) {
+            /*Delaying animation to show on first appear*/
+            setTimeout(() => {
+                /*Adding headers to left side*/
+
+                firstAppear = false;
+
+                leftSide.appendChild(h3);
+                leftSide.appendChild(h6);
+                
+                /*Adding message t right side*/
+                rightSide.textContent = this.message;
+                if (user == "antic") {
+                    rightSide.appendChild(deleteButton);
+                    deleteButton.className = "deleteButton"
+                    deleteButton.textContent = "X"
+                    deleteButton.onclick = () => {
+                        deleteRow(this)
+                    }
+                }
+                
+                /*Setting classes*/
+                h3.className = "leftSideH3 opacityChat";
+                h6.className = "leftSideH6 opacityChat";
+        
+                rightSide.className = "rightSide flex opacityChat";
+                
+
+                spanWrapper.style = "border-bottom: 1px solid rgba(6, 17, 97, 0.3)";
+
+                /*Scroll to bottom on load chat wrapper*/
+                var chatWindow = document.getElementById('mesagesWrapper'); 
+                var Height = chatWindow.scrollHeight; 
+                chatWindow.scrollTo(0, Height); 
+            
+                
+            }, 2200);
+        } else {
+            /*Adding headers to left side*/
+                leftSide.appendChild(h3);
+                leftSide.appendChild(h6);
+                /*Adding message t right side*/
+                rightSide.textContent = this.message;
+                if (user == "antic") {
+                    rightSide.appendChild(deleteButton);
+                    deleteButton.className = "deleteButton";
+                    deleteButton.textContent = "X";
+                    deleteButton.onclick = () => {
+                        deleteRow(this);
+                    }
+                }
+                
+                /*Setting classes*/
+                h3.className = "leftSideH3 opacityChat";
+                h6.className = "leftSideH6 opacityChat";
+        
+                rightSide.className = "rightSide flex opacityChat";
+
+                spanWrapper.style = "border-bottom: 1px solid rgba(6, 17, 97, 0.3)";
+        
+        }
+
+        return spanWrapper
+    }
+}
+
+
+/*Function to show messages on load,changes and delete...*/
+function showMessages() {
+
+    var chatWindow = document.getElementById('mesagesWrapper'); 
+    
+    var messageWrapper = document.getElementById("mesagesWrapper");
+    messageWrapper.innerHTML = "";
+        for (let i = 0; i < messagesArray.length; i++){
+            const span = messagesArray[i].oneMessage();
+            messageWrapper.appendChild(span);
+
+            /*Scroll to bottom every time*/
+            var Height = chatWindow.scrollHeight; 
+            chatWindow.scrollTo(0, Height); 
+        }
+}
+
+
+/*Set interval to refresh messages every x seconds*/
+function setInt() {
+    /*Wait for 3000ms and then call setInterval function*/
+    setTimeout(() => {
+        setInterval(() => {
+            getAllMessages();
+        }, 3000);
+    }, 3000);
+}
+
+
+
+/*************************************server Communication functions******************************************************* */
+/*Get all messages on login */
+function getAllMessages() {
+    axios.get("https://chat-expressjs.herokuapp.com/getAllMessages")
+    .then((res) => {
+        messagesArray = [];
+        for (let i = 0; i < res.data.messages.length; i++){
+            messagesArray.push( new UserMessage(res.data.messages[i].user, res.data.messages[i].time, res.data.messages[i].message))
+        }
+        showMessages();
+    })
+    .catch((error) => {
+        if (error.response) {
+            
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            
+            console.log(error.request);
+        } else {
+            
+            console.log('Error', error.message);
+        }
+    })
+        
+}
+
+
+
+/*On button click send message */
+function sendMessage(){
+    const date = new Date().getDate();
+    const day = new Date().getDay();
+    const year = new Date().getFullYear();
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes();
+
+    const time = date + "." + day + "." + year + " " + hours + ":" + minutes;
+    const message = document.getElementById("inputMessage").value
+    document.getElementById("inputMessage").value = "";
+
+    const data = {
+        user: user,
+        time: time,
+        message: message
+    }
+
+
+    axios.put("https://chat-expressjs.herokuapp.com/addMessage", data)
+    .then((res) => {
+        if (res.data.response) {
+            messagesArray = [];
+            for (let i = 0; i < res.data.response.length; i++){
+                messagesArray.push( new UserMessage(res.data.response[i].user, res.data.response[i].time, res.data.response[i].message))
+            }
+            showMessages();
+        }
+    })
+    .catch((error) => {
+        if (error.response) {
+            
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            
+            console.log(error.request);
+        } else {
+            
+            console.log('Error', error.message);
+        }
+    })
+}
+
+
+
+/*Delete one row*/
+function deleteRow(obj) {
+    console.log(obj.date)
+    console.log(obj.name)
+    console.log(obj.message)
+
+    axios.delete('https://chat-expressjs.herokuapp.com/delete', { data: obj })
+    .then((res) => {
+        messagesArray = [];
+        for (let i = 0; i < res.data.response.length; i++){
+            messagesArray.push( new UserMessage(res.data.response[i].user, res.data.response[i].time, res.data.response[i].message))
+        }
+        showMessages();
+    })
+    .catch((error) => {
+        if (error.response) {
+            
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            
+            console.log(error.request);
+        } else {
+            
+            console.log('Error', error.message);
+        }
+    })
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,280 @@
+/*******************************Code for form and first appear************************************************************ */
+
+
+var user;
 /*Start function to append form to wrapper, and later chat also*/
 function hello(child) {
     let wrapper = document.getElementById("wrapper");
-    wrapper.classList.add("flex")
-    wrapper.appendChild(child)
+    wrapper.classList.add("flex");
+    wrapper.appendChild(child);
 }
 
 
-/*setting up form function on load/refresh */
+/*setting up form *** on load/refresh */
 
 function form() {
+
+    /*Creating form*/
     let form = document.createElement("form");
     form.classList.add("form", "flex");
+    form.action = "https://chat-expressjs.herokuapp.com/logIn";
+    form.method = "post";
+    form.id = "form";
 
     /************************ */
+
+    /*Username form group**/
     let div1 = document.createElement("div");
+    div1.id = "formDiv1";
     div1.classList.add("formGroup", "top", "flex");
 
     let label1 = document.createElement("label");
     label1.className = "label";
     label1.textContent = "Username:"
+    label1.setAttribute('for', 'Username');
 
     let input1 = document.createElement("input");
     input1.className = "input";
-    input1.placeholder = "Type your username here!"
+    input1.placeholder = "Type your username here!";
+    input1.name = "Username";
+    input1.id = "Username";
+    input1.type = "text";
+
+    let small1 = document.createElement('small');
+    small1.classList.add("small", "flex","display");
+    small1.style = "line-height: normal";
+    small1.id = "small1"
+    small1.textContent = "Username doesn't exist!"
+
 
     div1.appendChild(label1);
     div1.appendChild(input1);
+    div1.appendChild(small1);
     
     /************************** */
 
-    /************************ */
+    /**********Password form group************** */
     let div2 = document.createElement("div");
-    div2.classList.add("formGroup", "flex")
+    div2.id = "formDiv2";
+    div2.classList.add("formGroup", "flex");
 
     let label2 = document.createElement("label");
     label2.className = "label";
     label2.textContent = "Secret code:"
+    label2.setAttribute('for', 'code');
 
     let input2 = document.createElement("input");
     input2.className = 'input';
-    input2.placeholder = "Type your secret code here!"
+    input2.placeholder = "Type your secret code here!";
+    input2.name = "code";
+    input2.id = "code";
+    input2.type = "password";
+
+    let small2 = document.createElement('small');
+    small2.classList.add("small", "flex","display");
+    small2.style = "line-height: normal";
+    small2.id = "small2"
+    small2.textContent = "Secret code doesn't match!"
 
     div2.appendChild(label2);
     div2.appendChild(input2);
+    div2.appendChild(small2);
 
-    /************************** */
+    /********* Button form group***************** */
+    let divButton = document.createElement("div");
+    divButton.classList.add("formGroup", "flex");
+    divButton.id = "formDivButton";
 
-    let button = document.createElement("button");
+    var button = document.createElement("button");
     button.className = "button";
     button.textContent = "Go to chat!";
+    button.id = "buttonForm";
+    button.type = "submit";
+    let small3 = document.createElement('small');
+    small3.classList.add("small", "flex","display");
+    small3.style = "line-height: normal; width:300px;";
+    small3.id = "small3"
+    small3.textContent = "Invalid credentials!";
+
+    divButton.appendChild(button);
+    divButton.appendChild(small3);
+
 
     form.appendChild(div1);
     form.appendChild(div2);
-    form.appendChild(button);
+    form.appendChild(divButton);
+    
+    /*Adding event listener on submit*/
+    form.addEventListener('submit', (event) => {
+        document.getElementById("small3").classList.add("display")
+        validateForm(event)
+    })
     return form;
 }
 
 
+
 /*Function on form button click t check validation*/
-function checkIfValid(){
-    console.log("clicked")
+function validateForm(event) {
+    /*Preventing default to stop submit, so we can check is input are not empty etc.*/
+    event.preventDefault(); 
+
+    const a = event.target['Username'].value;
+    const b = event.target['code'].value;
+
+
+    var small1 = document.getElementById("small1");
+    var small2 = document.getElementById("small2");
+
+    if (a.length == 0) {
+        small1.textContent = "Please enter your username!";
+        small1.classList.remove("display");
+    } else {
+        small1.classList.add("display");
+    }
+
+    if (b.length == 0) {
+        small2.textContent = "Please enter secret code!";
+        small2.classList.remove("display");
+    } else {
+        small2.classList.add("display");
+    }
+
+    if (a.length != 0 && b.length != 0) {
+        /*Calling server to check username and secret code*/
+        callServer(a, b)
+    }
+}
+
+/*calling a server on form submit **************/
+
+/***************************Using XMLHttpRequest************************************************ 
+function callServer(val1, val2) {
+    var data = JSON.stringify({ val1: val1, val2: val2 });
+
+    const xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open("POST", "https://chat-expressjs.herokuapp.com/logIn");
+
+    xmlHttp.onreadystatechange = function(){
+        
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.response)
+        }
+        
+    }
+    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlHttp.send(data);
+}
+*************************************************************************************************** */
+
+
+
+/************************Using Promises****************************************************** 
+function callServer(val1, val2) {
+    var data = JSON.stringify({ val1: val1, val2: val2 });
+    return new Promise((resolve, reject) => {
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.open('POST', 'https://chat-expressjs.herokuapp.com/logIn');
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                var a = JSON.parse(this.response)
+                resolve(a)
+            } else {
+                reject(this)
+            }
+            }
+        }
+
+        xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xmlHttp.send(data)
+    })
+}
+Then where we call the function we can use this...:
+.then((res, rej) => {
+            console.log(res)
+            })
+            .catch((e)=>{
+            console.log((e))
+            })
+            })
+********************************************************************************************/
+/*****************************Using Fetch and async await***************************************************************/
+/*
+async function callServer(val1, val2) {
+    var data = JSON.stringify({ val1: val1, val2: val2 });
+    var object = {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body:data
+    }
+    try {
+        const response = await fetch('https://chat-expressjs.herokuapp.com/logIn', object);
+
+        if (!response.ok) throw new Error(`Http response status: ${response.status}`);
+
+        const res = await response.json()
+
+        console.log(res)
+    } catch (e) {
+        console.log(e)
+    }
+}
+***************************************************************************************/
+
+
+/*********************************USING axios******************************************************/
+
+async function callServer(val1, val2) {
+    /*Data to send*/
+    var data = JSON.stringify({ val1: val1, val2: val2 });
+
+    try {
+        var response = await axios.post('https://chat-expressjs.herokuapp.com/logIn', data,{headers: {
+        // Overwrite Axios's automatically set Content-Type
+        'Content-Type': 'application/json'
+        }
+        });
+
+        if (response.data.status == "OK") {
+            /*Setting user name after success*/
+            user = response.data.user;
+            /*calling function to set display and animations*/
+            addAnimation();
+
+        } else {
+            document.getElementById("small3").classList.remove("display");
+            throw new Error(`Username or Secret code dont match. Your values: ${response.data.user} and ${response.data.code}` )
+        }
+        
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('Error', error.message);
+        }
+        console.log(error);
+    }
+}
+
+function addAnimation() {
+    /*Adding animation*/
+    document.getElementById("form").classList.add("animation");
+    document.getElementById("formDivButton").classList.add("opacity1");
+    document.getElementById("formDiv1").classList.add("opacity1");
+    document.getElementById("formDiv2").classList.add("opacity2");
+    setTimeout(() => {
+        document.getElementById("form").classList.add("display");
+        /*Function createChat is in another .js file and is used here to append chat to wrapper*/
+        hello(createChat())
+    }, 2300);
 }
 
 
